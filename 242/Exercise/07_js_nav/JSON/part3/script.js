b937f84bff2798d5780523ce281d05f6a0a7c530
KageCL <cxlowery@email.sc.edu>
2024-10-10 19:06:17 -0400

    Added 10_json to main index

--- 242/Exercise/07_js_nav/JSON/part3/script.js
+++ 242/Exercise/07_js_nav/JSON/part3/script.js
@@ -1,1 +1,33 @@
-//"https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+const getCocktails = async() => {
+  
+
+  try {
+      const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita").json();
+      return response.json();
+  } catch(error){
+      console.log(error);
+  }
+};
+
+const showCocktails = async() => {
+  const response = await getCocktails();
+  const cocktails = response.drinks;
+  
+  cocktails.forEach((cocktail)=>{
+      document.getElementById("cocktail-grid").append(getCocktailSection(cocktail));
+  });
+};
+
+const getCocktailSection = (cocktail) => {
+  const section = document.createElement("section");
+
+  const cocktailImage = document.createElement("img");
+  cocktailImage.src = cocktail.strImageSource;
+  section.append(cocktailImage);
+
+  return section;
+}
+
+
+//show all of the shoes when the page loads
+showCocktails();
