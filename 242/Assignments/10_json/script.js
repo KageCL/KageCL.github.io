d48529fc650bb565d26c09c6100c9f70bf0e8d22
KageCL <cxlowery@email.sc.edu>
2024-10-10 18:36:15 -0400

    Completed Assignment 10_json

--- 242/Assignments/10_json/script.js
+++ 242/Assignments/10_json/script.js
@@ -1,10 +1,10 @@
 const getIcecream = async() => {
-    const url = "https://portiaportia.github.io/json/ice-creams.json"
+    const url = "https://portiaportia.github.io/json/ice-creams.json";
 
-    try{
+    try {
         const response = await fetch(url);
         return response.json();
-    } catch(error){
+    } catch (error) {
         console.log(error);
     }
 };
@@ -12,11 +12,25 @@ const getIcecream = async() => {
 const showIcecream = async() => {
     const icecreams = await getIcecream();
 
-    icecreams.forEach((icecream)=>{
-        document.getElementById("icecream-section").append(getIcecream(icecream));
-    })
-}
+    icecreams.forEach((icecream) => {
+        document.getElementById("icecream-section").append(getIcecreamSection(icecream));
+    });
+};
 
 const getIcecreamSection = (icecream) => {
     const section = document.createElement("section");
-}
+  
+    const sweetImage = document.createElement("img");
+    sweetImage.src = `https://portiaportia.github.io/json/images/ice-creams/${icecream.image}`;
+    section.append(sweetImage);
+  
+    const overlay = document.createElement("div");
+    overlay.className = "overlay";
+    overlay.textContent = icecream.name;
+    section.append(overlay);
+  
+    return section;
+  };
+  
+
+showIcecream();
